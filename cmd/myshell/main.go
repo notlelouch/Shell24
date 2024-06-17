fa74475a1169f68fd4075ccf4b16eec989f1cc7f
notlelouch <notlelouchvb@gmail.com>
2024-06-18 03:35:32 +0530

    implemented type builtin: builtins

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,6 +9,9 @@ import (
 func main() {
 
 	var exit bool = false
+
+	shellBuiltinCommands := map[string]int{"type": 1, "exit": 1, "pwd": 1, "echo": 1}
+	var pass int = 0
 	for {
 		if !exit {
 			fmt.Fprint(os.Stdout, "$ ")
@@ -25,8 +28,21 @@ func main() {
 
 			if input[:4] == "echo" {
 				fmt.Fprintf(os.Stdout, "%s\n", input[5:])
-			} else {
+				pass = 1
+			}
+
+			if input[:4] == "type" {
+				if shellBuiltinCommands[input[5:]] == 1 {
+					fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", input[5:])
+				} else {
+					fmt.Fprintf(os.Stdout, "%s: not found\n", input[5:])
+				}
+				pass = 1
+			}
+
+			if pass != 1 && pass == 0 {
 				fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+				pass = 0
 			}
 
 		}
