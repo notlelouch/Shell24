ae9e02dac4150aeb3d5042d33136955f96ea553b
notlelouch <notlelouchvb@gmail.com>
2024-06-22 23:27:41 +0530

    implemented type builtin: executable files

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,47 +4,56 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 
-	var exit bool = false
+	// trimmed_cons := strings.TrimRight(os.Args[0], "\r\n")
+	// trimmed_cons := strings.TrimSuffix(os.Args[0], "\r\n$")
 
-	shellBuiltinCommands := map[string]int{"type": 1, "exit": 1, "pwd": 1, "echo": 1}
-	var pass int = 0
+	paths := []string{}
+	// path := os.Args[0]
+
+	path := os.Getenv("PATH")
+	// /fmt.Println("Value of 'PATH' environment variable:", path)
+	pathParts := strings.Split(path, ":")
+	paths = append(paths, pathParts...)
+
+	//fmt.Fprint(os.Stdout, paths)
+
+	shellBuiltinCommands := map[string]int{"type": 1, "exit": 1, "pwd": 1, "echo": 1, "cd": 1}
 	for {
-		if !exit {
-			fmt.Fprint(os.Stdout, "$ ")
+		fmt.Fprint(os.Stdout, "$ ")
 
-			input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-			if err != nil {
-				fmt.Println("Error reading", err)
-			}
-			input = input[:len(input)-1]
-
-			if input == "exit 0" {
-				os.Exit(0)
-			}
-
-			if input[:4] == "echo" {
-				fmt.Fprintf(os.Stdout, "%s\n", input[5:])
-				pass = 1
-			}
-
-			if input[:4] == "type" {
-				if shellBuiltinCommands[input[5:]] == 1 {
-					fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", input[5:])
-				} else {
-					fmt.Fprintf(os.Stdout, "%s: not found\n", input[5:])
-				}
-				pass = 1
-			}
-
-			if pass != 1 && pass == 0 {
-				fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
-				pass = 0
-			}
-
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading", err)
 		}
+		input = input[:len(input)-1]
+
+		switch {
+		case input == "exit 0":
+			os.Exit(0)
+		case input[:4] == "echo":
+			fmt.Fprintf(os.Stdout, "%s\n", input[5:])
+		case input[:4] == "type":
+			if shellBuiltinCommands[input[5:]] == 1 {
+				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", input[5:])
+			} else if shellBuiltinCommands[input[5:]] != 1 {
+				for _, path := range paths {
+					if _, err := os.Stat(path + "/" + input[5:]); !os.IsNotExist(err) {
+						fmt.Fprintf(os.Stdout, "%s is %s\n", input[5:], path+"/"+input[5:])
+						break
+					}
+				}
+			} else {
+				fmt.Fprintf(os.Stdout, "chuuuuuuuuuuttt")
+				fmt.Fprintf(os.Stdout, "%s: not found\n", input[5:])
+			}
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+		}
+
 	}
 }
